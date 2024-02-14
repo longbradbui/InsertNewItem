f8561c2ea19fc69dfa58a24a088ec4d005692b13
Hoang Long Bui <86465921+longbradbui@users.noreply.github.com>
2024-02-13 19:53:41 -0800

    Updated list.h

--- list.h
+++ list.h
@@ -1,83 +1,30 @@
-#ifndef LIST_H_
-#define LIST_H_
+#ifndef DOUBLY_LINKED_H_
+#define DOUBLY_LINKED_H_
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Node
+{
+    Node *next;
+    Node *previous;
+    int data;
+};
 class List
 {
-public: 
-	struct Node
-	{
-		Item* pItem;
-		Node* previous;
-		Node* next;
-	};
-	List();
-	~List();
-	bool InsertNewItem(Item& it);
+public:
+    List();
+    List(const List& parent_list);
+    ~List();
+    bool InsertItem(int value);
+    bool RemoveItem(int value);
+    bool Push(int value);
+    bool Pop();
+    List& operator=(const List& parent_list);
+    friend ostream &operator<<(ostream &stream, const List &rhs);
+
 private:
-	Node* head_ptr_ = nullptr;
-	Node* tail_ptr_ = nullptr;
+    Node *tail_ptr_;
+    Node *head_ptr_;
 };
 #endif
-
-bool List::InsertNewItem(Item& it)
-{
-	Node* insert_node = new Node();
-	insert_node->next = nullptr;
-	insert_node->previous = nullptr;
-	insert_node->pItem = &(it); // get the value
-	if (head_ptr_ == nullptr)
-	{
-		head_ptr_ = insert_node;
-		insert_node->previous = head_ptr_;
-		insert_node->next = tail_ptr_;
-		return true;
-	}
-	if (*(head_ptr_->pItem) == it)
-	{
-		delete insert_node;
-		insert_node = nullptr;
-		return false;
-	}
-	if (*(head_ptr_->pItem) > it)
-	{
-		insert_node->next = head_ptr_;
-		head_ptr_ = insert_node;
-		return true;
-	}
-	if (*(tail_ptr_->pItem) < it)
-	{
-		insert_node->previous = tail_ptr_;
-		tail_ptr_->next = insert_node;
-		tail_ptr_ = insert_node;
-		return true;
-	}
-	Node* current_node = head_ptr_;
-	while (current_node != nullptr) 
-	{
-		if (*(current_node->pItem) == it)
-		{
-			delete insert_node;
-			insert_node = nullptr;
-			return false;
-		}
-		else if (*(current_node->pItem) < it)
-		{
-			current_node = current_node->next;
-		} 
-		else
-		{
-			/* assigning the previous pointer of insert_node to the same
-			node that current_node is pointing to as its previous */
-			insert_node->previous = current_node->previous;
-			/* next pointer of insert_node is set to point to current_node, indicating 
-			that current_node will be the next node in the list after insert_node. */
-			insert_node->next = current_node;
-			/* This line updates the next pointer of the node before current_node (i.e., current_node->previous)
-			to point to insert_node, effectively linking insert_node into the list .*/
-			current_node->previous->next = insert_node;
-			/* the previous pointer of current_node is updated to point back to insert_node, 
-			ensuring the proper bidirectional linking of nodes in the list. */
-			current_node->previous = insert_node;
-			return true;
-		}		
-	}
-}
